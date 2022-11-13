4db9374842ea449ead1667d4e0b9392cb5857247
fabiotano <fabio.capriotti@gmail.com>
2022-11-13 16:13:42 -0300

    creo array autos

---
+++ autos.js
@@ -0,0 +1,39 @@
+let autos = [
+    {
+    marca: "Ford",
+    modelo: "Fiesta",
+    precio: 150000, //150k
+    km: 200,
+    color: "azul", 
+    cuotas: 12,
+    anio: 2019,
+    patente: "APL123",
+    vendido: false,
+},
+
+{
+    marca: "Toyota",
+    modelo: "Corolla",
+    precio: 50000, //50k
+    km: 0,
+    color: "azul", 
+    cuotas: 14,
+    anio: 2019,
+    patente: "JJK116",
+    vendido: false,
+},
+
+{
+    marca: "Ferrari",
+    modelo: "Testarossa",
+    precio: 99000, //100k
+    km: 0,
+    color: "rojo", 
+    cuotas: 10,
+    anio: 2022,
+    patente: "JJK116",
+    vendido: false,
+},
+]
+
+module.exports = autos;
